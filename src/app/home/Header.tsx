269003b054bf79fa2f5e062812ba6a3d80a2a512
Arvin Xu <arvinx@foxmail.com>
2024-07-03 12:53:37 +0800

    Update Header.tsx

--- src/app/home/Header.tsx
+++ src/app/home/Header.tsx
@@ -6,21 +6,15 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
-import { DISCORD_URL, GITHUB_REPO, MJ_DOCS } from '@/const/url';
+import { MJ_DOCS } from '@/const/url';
 import Settings from '@/features/Settings';
 
 const Header = memo(() => {
   const { t } = useTranslation('common');
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'}>
-      <Logo extra={<Midjourney.Combine size={32} />} type={'combine'} />
+      <Midjourney.Combine size={32} />
       <Flexbox horizontal>
-        <Link href={DISCORD_URL} style={{ color: 'inherit' }} target={'_blank'}>
-          <ActionIcon icon={DiscordIcon} title={'Discord'} />
-        </Link>
-        <Link href={GITHUB_REPO} style={{ color: 'inherit' }} target={'_blank'} title={'Github'}>
-          <ActionIcon icon={LucideGithub} />
-        </Link>
         <Link href={MJ_DOCS} style={{ color: 'inherit' }} target={'_blank'} title={t('docs')}>
           <ActionIcon icon={Book} />
         </Link>
