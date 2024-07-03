4c529d40a47e8ec0467fc49b3a5485fa9492314d
Arvin Xu <arvinx@foxmail.com>
2024-07-03 15:00:45 +0800

    Update metadata.ts

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -12,35 +12,11 @@ const { METADATA_BASE_URL } = getServerConfig();
 const metadataBase = METADATA_BASE_URL ? new URL(METADATA_BASE_URL!) : undefined;
 
 const metadata: Metadata = {
-  appleWebApp: {
-    statusBarStyle: 'black-translucent',
-    title,
-  },
-  description,
-  manifest: '/manifest.json',
   metadataBase,
-  openGraph: {
-    description: description,
-    locale: 'en-US',
-    siteName: title,
-    title: title,
-    type: 'website',
-    url: homepage,
-  },
-
   title: {
     default: title,
     template: '%s · Midjourney WebUI',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    creator: '@lobehub',
-    description,
-    images: [
-      'https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/og-960x540.png',
-    ],
-    title,
-  },
+  }
 };
 
 export default metadata;
